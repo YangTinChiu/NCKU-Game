da04ce7628e91a297a8bcec062ff5155d4847a97
Solo-steven <ssteven19@kimo.com>
2024-01-03 23:08:23 +0800

    feat: impl enemy animation

--- Assets/Script/John_animator.cs
+++ Assets/Script/John_animator.cs
@@ -24,18 +24,14 @@ public class John_animator : MonoBehaviour
         if(Input.GetKey(KeyCode.Z)) {
             // perform animation
             animator.SetInteger("isFight",1);
-            Debug.Log("Geo" + Geometry.transform.position.ToString());
             Collider[] enemyInScope = Physics.OverlapSphere(Geometry.transform.position, attackRange, EnemyLayer);
-            Debug.Log("We Hit : " + enemyInScope.Length.ToString());
             foreach (Collider enemy in enemyInScope) {
-                Debug.Log("We Hit");
+                enemycontrol enemyControl = enemy.GetComponent<enemycontrol>();
+                enemyControl.Damage();
             }
         }
         else {
             animator.SetInteger("isFight",0);
         }
-    }
-    void OnDrawGizmosSelected() {
-        Gizmos.DrawWireSphere(Geometry.transform.position, attackRange);
     }
 }
--- Assets/Script/enemycontrol.cs
+++ Assets/Script/enemycontrol.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 
 public class enemycontrol : MonoBehaviour
 {
+    [SerializeField] Animator animator;
     [SerializeField] GameObject player;
     [SerializeField] float movingSpeed = 2;
-    [SerializeField] float maxdetectdistance = 30;
+    [SerializeField] float maxdetectdistance = 10;
 
     int Life = 100;
 
@@ -17,12 +18,18 @@ public class enemycontrol : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    void FixedUpdate() {
+        if(animator.GetInteger("State") == 3) {
+            return;    
+        }
         playerTransform = player.transform;
         t = transform;
 
@@ -30,15 +37,19 @@ public class enemycontrol : MonoBehaviour
 
         if((2 <= distance) && (distance <= maxdetectdistance))
         { 
-            Vector3 movingDirction = new Vector3(playerTransform.position.x - t.position.x, 0, playerTransform.position.z - t.position.z).normalized;
-
+            Vector3 movingDirction = new Vector3(playerTransform.position.x - t.position.x, 0,playerTransform.position.z - t.position.z).normalized;
             t.localPosition += movingSpeed * Time.deltaTime * movingDirction;
+            animator.SetInteger("State", 1);
+        }else if ((2 >= distance) && (distance <= maxdetectdistance)){
+            animator.SetInteger("State", 2);
+        }else {
+            animator.SetInteger("State", 0);
         }
-        
     }
     public bool Damage() {
         Life -= 10;
         if(Life==0) {
+            animator.SetInteger("State", 3);
             return true;
         }
         return false;
